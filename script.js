9f3afc190a31bd1d2b3159674bbf9325efe8826f
Ameer <ameer-hellaly@gmail.com>
2024-09-28 18:32:23 +0300

    add ameer folder

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ document.querySelector(".ameer").addEventListener("click", () => {
 document.querySelector(".aya").addEventListener("click", () => {
   window.location.href = "ayaSareej/index.html";
 });
+document.querySelector(".ameer").addEventListener("click", () => {
+  window.location.href = "ameer/index.html";
+});
